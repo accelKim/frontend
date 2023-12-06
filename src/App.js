8a57d50b20a725f6bce2371704a2eb3092055bac
accelKim <dnjsgh567877@gmail.com>
2023-12-06 20:13:11 +0900

    Revert "12.04"
    This reverts commit 872f0c64c294d039317b8400d8c2520624889344.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import InstructorApp from './component/InstructorApp';
+=======
+import Login from './login/Login'
+
+>>>>>>> parent of 872f0c6 (12.04)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <InstructorApp />
+=======
+      <Login />
+>>>>>>> parent of 872f0c6 (12.04)
     </div>
   );
 }
