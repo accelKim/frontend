8692f1c2501ed660b59c3f011aeadcc543557cbb
accelKim <dnjsgh567877@gmail.com>
2023-12-06 17:50:55 +0900

    12.06

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import InstructorApp from './component/InstructorApp';
+import Home from './home/home'
 
 function App() {
   return (
     <div className="App">
-      <InstructorApp />
+      <Home />
     </div>
   );
 }
